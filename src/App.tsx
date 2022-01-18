f80a36012842215ac5822c594699928060ba0d7f
yesenia <ymartinezac@uoc.edu>
2022-01-18 19:38:21 -0400

    top bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import './styles.css'
 import './components/someter-medida/someter-medida.css';
 import SometerMedidaForm from './components/someter-medida/someter-medida';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 const theme = createTheme({
   palette: {
-    type: 'light',
+
     primary: {
         main: '#2e4c32',
       },
@@ -20,11 +27,31 @@ const theme = createTheme({
     fontFamily:'Roboto',
     fontSize: 14,    
   },
+  spacing: 4
 });
 function App()
 {
     return (
         <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                    {/*<IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton> */}
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Cámara de Representantes
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
         <div className='App'>
             <h1>Radicación de Medidas</h1>
             <SometerMedidaForm />
--- src/components/someter-medida/someter-medida.tsx
+++ src/components/someter-medida/someter-medida.tsx
@@ -3,9 +3,27 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Dropzone from 'react-dropzone';
 import Card from '@mui/material/Card';
-//import LoadingButton from '@mui/lab/LoadingButton';
-//import SaveIcon from '@mui/icons-material/Save';
-
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#2e4c32',
+      },
+      secondary: {
+        main: '#d7bc39',
+      },
+      success: {
+        main: '#17545b',
+      },
+  },
+  typography: {   
+    fontFamily:'Roboto',
+    fontSize: 14,    
+  },
+  spacing: 4
+});
 
 async function postMedida(data){
   const medidas_url = "http://localhost:9000/medidas";
@@ -14,8 +32,6 @@ async function postMedida(data){
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   };
-
-
   fetch(medidas_url, postRequestOptions)
       .then(async response => {
           const isJson = response.headers.get('content-type')?.includes('application/json');
@@ -27,6 +43,7 @@ async function postMedida(data){
             //  const error = (data && data.message) || response.status;
               //return Promise.reject(error);
           }
+             
       })
       .catch(error => { 
           console.error('There was an error!', error);
@@ -36,7 +53,8 @@ async function postMedida(data){
 function SometerMedidaForm() {
     
     const [valueTipoMedida, setValueTipoMedida] = React.useState<string|undefined>('');
-    const [valueMedidaFile, setValueMedidaFile] = React.useState<Blob|File|undefined|null>(undefined);
+    const [valueMedidaFile, setValueMedidaFile] = React.useState<File|undefined|null>(undefined);
+     
     
     const tipo_medidas = [
       //'p_de_la_c', 'r_de_la_c', 'rc_de_la_c', 'r_conc_de_la_c', 'voto_explicativo', 'plan_de_reorganizacion'
@@ -79,10 +97,12 @@ function SometerMedidaForm() {
     postMedida(data);
   };
   return (
-    <Card className="upload-medida-card" variant="outlined">
+   
+    <Card className="upload-medida-card">
       <form onSubmit={handleSubmit}>
           <Autocomplete
             disablePortal
+            size="small"
             id="combo-box-demo"
             options={tipo_medidas}
             getOptionLabel={(tipo) => tipo.label}
@@ -103,21 +123,19 @@ function SometerMedidaForm() {
                 <div className="dropzone" {...getRootProps()}>
                   <input {...getInputProps()} />
                   <p>Drag 'n' drop some files here, or click to select files</p>
+                  { valueMedidaFile ? <p>{valueMedidaFile.name}</p> : null}
                 </div>
               </section>
             )}
           </Dropzone>
-        <button type="submit" className="btn btn-success" >ADD</button>
-       {/*} <LoadingButton
-            loading
-            color="secondary"
-            loadingPosition="start"
-            startIcon={<SaveIcon />}
-            variant="outlined">
-            Save
-            </LoadingButton> */}
+        
+          <Button variant="contained" type="submit" color="primary" endIcon={<SaveIcon />}>
+            Guardar
+          </Button>
+        
       </form>       
     </Card>
+
   );
 }
 
